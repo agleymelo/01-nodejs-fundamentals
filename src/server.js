c3b3d68a8942950655cd0417be10701ad6f8760e
Agleylson Melo <agleylsonmelo@outlook.com>
2024-03-22 22:20:45 -0300

    Rotas de criação e listagem (Métodos HTTP)

--- src/server.js
+++ src/server.js
@@ -1,6 +1,18 @@
 import http from 'node:http'
 
+// 
+
 const server = http.createServer((request, response) => {
+  const { method, url } = request
+
+  if (method === "GET" && url === "/users") {
+    return response.end("listagem de usuarios")
+  }
+
+  if (method === "POST" && url === "/users") {
+    return response.end("criar usuario")
+  }
+
   return response.end("Hello Ignite!")
 })
 
