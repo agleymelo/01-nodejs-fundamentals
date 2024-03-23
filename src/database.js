c24a37de0a5f2327acf3e6c21023d02082573388
Agleylson Melo <agleylsonmelo@outlook.com>
2024-03-23 11:04:48 -0300

    Atualização de registros

--- src/database.js
+++ src/database.js
@@ -1,15 +1,17 @@
 import fs from 'node:fs/promises'
 
-const databasePath = new URL("../db.json", import.meta.url)
+const databasePath = new URL('../db.json', import.meta.url)
 export class Database {
   #database = {}
 
   constructor() {
-    fs.readFile(databasePath, "utf-8").then(data => {
-      this.#database = JSON.parse(data)
-    }).catch(() => {
-      this.#persist()
-    })
+    fs.readFile(databasePath, 'utf-8')
+      .then((data) => {
+        this.#database = JSON.parse(data)
+      })
+      .catch(() => {
+        this.#persist()
+      })
   }
 
   #persist() {
@@ -17,7 +19,6 @@ export class Database {
   }
 
   select(table) {
-
     const data = this.#database[table] ?? []
 
     return data
@@ -35,13 +36,21 @@ export class Database {
     return data
   }
 
-  delete(table, id) {
+  update(table, id, data) {
+    const rowIndex = this.#database[table].findIndex((row) => row.id === id)
 
-    const rowIndex = this.#database[table].findIndex(row => row.id === id)
+    if (rowIndex > -1) {
+      this.#database[table][rowIndex] = { id, ...data }
+      this.#persist()
+    }
+  }
+
+  delete(table, id) {
+    const rowIndex = this.#database[table].findIndex((row) => row.id === id)
 
     if (rowIndex > -1) {
       this.#database[table].splice(rowIndex, 1)
       this.#persist()
     }
   }
-} 
+}
