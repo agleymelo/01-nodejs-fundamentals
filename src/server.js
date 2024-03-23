5092da6acbe61e263cda56c601e02355c4fc9b3f
Agleylson Melo <agleylsonmelo@outlook.com>
2024-03-22 22:28:14 -0300

    Salvando usuários em memória (Headers)

--- src/server.js
+++ src/server.js
@@ -1,19 +1,25 @@
 import http from 'node:http'
 
-// 
+const users = []
 
 const server = http.createServer((request, response) => {
   const { method, url } = request
 
-  if (method === "GET" && url === "/users") {
-    return response.end("listagem de usuarios")
+  if (method === 'GET' && url === '/users') {
+    return response.setHeader('Content-Type', 'application/json').end(JSON.stringify(users))
   }
 
-  if (method === "POST" && url === "/users") {
-    return response.end("criar usuario")
+  if (method === 'POST' && url === '/users') {
+    users.push({
+      id: 1,
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+    })
+
+    return response.end('criar usuario')
   }
 
-  return response.end("Hello Ignite!")
+  return response.end('Hello Ignite!')
 })
 
 server.listen(3333)
