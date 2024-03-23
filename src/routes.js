58f167464a6fcdfe2150c3777b2845c7e267e913
Agleylson Melo <agleylsonmelo@outlook.com>
2024-03-23 10:36:02 -0300

    Criando regex dos parâmetros

--- src/routes.js
+++ src/routes.js
@@ -1,34 +1,40 @@
-import { randomUUID } from 'node:crypto'
+import { randomUUID } from "node:crypto";
 
-import { Database } from './database.js'
+import { Database } from "./database.js";
+import { buildRoutePath } from "./utils/build-route-path.js";
 
-const database = new Database()
+const database = new Database();
 
 export const routes = [
   {
-    method: 'GET',
-    path: '/users',
+    method: "GET",
+    path: buildRoutePath("/users"),
     handler: async (request, response) => {
-      const users = database.select('users')
+      const users = database.select("users");
 
-      return response.end(JSON.stringify(users))
+      return response.end(JSON.stringify(users));
     },
   },
   {
-    method: 'POST',
-    path: '/users',
+    method: "POST",
+    path: buildRoutePath("/users"),
     handler: async (request, response) => {
-      const { name, email } = request.body
+      const { name, email } = request.body;
 
       const user = {
         id: randomUUID(),
         name,
         email,
-      }
+      };
 
-      database.insert('users', user)
+      database.insert("users", user);
 
-      return response.writeHead(201).end()
+      return response.writeHead(201).end();
     },
   },
-]
+  {
+    method: "DELETE",
+    path: buildRoutePath("/users/:id"),
+    handler: (request, response) => {},
+  },
+];
